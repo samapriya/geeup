c43827fc9331095cf30de8837a80a9a38146c26c
samapriya <samapriya.roy@gmail.com>
2018-08-11 01:13:46 -0400

    Updated v0.0.3

--- setup.py
+++ setup.py
@@ -18,13 +18,14 @@ def readme():
         return f.read()
 setuptools.setup(
     name='geeup',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
     url='https://github.com/samapriya/geeup',
     install_requires=['earthengine_api >= 0.1.87','requests >= 2.10.0','hurry.filesize>=0.9','retrying >= 1.3.3','beautifulsoup4 >= 4.5.1',
                       'requests_toolbelt >= 0.7.0','pytest >= 3.0.0','future >= 0.16.0','google-cloud-storage >= 1.1.1'],
     license='Apache 2.0',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
